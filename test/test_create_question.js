7426d803ca83ad511d8641426469b6ee3caf5888
kiutter <kutter18@student.oulu.fi>
2019-04-24 16:12:28 +0300

    Tests for question-model

--- test/test_create_question.js
+++ test/test_create_question.js
@@ -1,24 +1,26 @@
 var assert = require("assert");
-var expect = require("chai").expect;
-var should = require("chai").should();
+//var expect = require("chai").expect;
+//var should = require("chai").should();
 var models = require("../src/models/models");
 
+//Tests for question creation process:
 describe("Creating questions", () => {
+	//Lets test to create a completely new question to database:
 	it("Saves a new question", done => {
 		var q1 = new models.Question({ question: "test question", author: "testbot" });
 		q1.save().then(() => {
-			assert(!q1.isNew);
+			assert(!q1.isNew); //Condition is returned as true, if new question was created (note: isNew returns false, so !q1.isNew = true if question was created)
 			done();
 		});
 	});
+	//Lets test an error scenario. question parameter should not be allowed to be empty and thus cause an error:
 	it("Creates a invalid question and returns error", done => {
 		var q1 = new models.Question({ question: "", author: "testbot" });
 		q1.save(function(err) {
 			if (err) {
-				done();
+				done(); //error was thrown as expected, complete test.
 			} else {
-				console.log("This should give an error!!!");
-				done(err);
+				console.warn("Empty question paramater didn't give error!"); // Error if no error was thrown as expected.
 			}
 		});
 	});
--- test/test_read_question.js
+++ test/test_read_question.js
@@ -1,28 +1,46 @@
 var assert = require("assert");
 var assert = require("assert");
-var expect = require("chai").expect;
-var should = require("chai").should();
 var models = require("../src/models/models");
 
+//Creates a new question to database, which we can read later.
 let q1;
 beforeEach(done => {
 	q1 = new models.Question({ question: "test question2", author: "testbot2" });
 	q1.save().then(() => {
+		//lets check it was created:
 		assert(!q1.isNew);
 		done();
 	});
 });
 
-describe("Reading the question", () => {
-	it("Looks if the question exists", done => {
+//Create a new test for reading the question
+describe("Reading a question", () => {
+	it("Search question by question-string", done => {
+		//actual test
 		models.Question.find({ question: "test question2" }, (err, question) => {
+			//find the question according to the question string
 			if (err) {
 				throw err;
 			}
 			if (question.length === 0) {
-				throw new Error("Question wasn not created!");
+				throw new Error("Could not read a question or it doesn't exist!"); //error if the question is not found
 			}
-			done();
+			done(); //Error wasn't thrown, so everything went ok.
+		});
+	});
+	//lets test to search for question that doesn't exists. Shouldn't return anything.
+	it("Search question that can't be found, shouldn't return anything", done => {
+		//actual test
+		models.Question.find({ question: "can't find me" }, (err, question) => {
+			//find the question according to the question string
+			if (err) {
+				throw err;
+			}
+			if (question.length === 0) {
+				done(); //test is done, if nothing is returned.
+			} else {
+				throw new Error("Something was found, this shouldn't happen!!"); //error if the question is found
+			}
 		});
 	});
 });
